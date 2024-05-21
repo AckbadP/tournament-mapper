bae6c1281f7d30d1b53f392222aa98269fc21649
Ian Manix <AckbadP@gmail.com>
2024-05-21 15:37:18 -0700

    added func to draw bounding boxes around text

--- vid-reader/reader2.py
+++ vid-reader/reader2.py
@@ -3,9 +3,12 @@ import pytesseract
 from pytesseract import Output
 
 PATH = "data/"
+OUT = PATH+"out.png"
 EASY = PATH + "easy1.png"
 EASY_TWO = PATH + "easy2.png"
+EASY_TEST = PATH + "test1.png"
 
+# ref: https://www.datacamp.com/tutorial/optical-character-recognition-ocr-in-python-with-pytesseract
 def image_to_text(input_img):
     '''
     A func to read text from inages
@@ -26,9 +29,70 @@ def image_to_data(input_img):
 
     return data
 
+def draw_bounding_boxes(img_in, img_out):
+    img = cv2.imread(img_in)
+
+   # Extract data
+    data = image_to_data(img_in)
+    n_boxes = len(data["text"])
+
+    for i in range(n_boxes):
+        if data["conf"][i] == -1:
+            continue
+        # Coordinates
+        x, y = data["left"][i], data["top"][i]
+        w, h = data["width"][i], data["height"][i]
+
+       # Corners
+        top_left = (x, y)
+        bottom_right = (x + w, y + h)
+
+        # Box params
+        green = (0, 255, 0)
+        thickness = 3  # The function-version uses thinner lines
+
+        cv2.rectangle(img, top_left, bottom_right, green, thickness)
+
+    # Save the image with boxes
+    cv2.imwrite(img_out, img)
+
+def grayscale(img):
+    '''
+    convert image to grayscale
+    '''
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 print("------ Easy 1 ------")
 print(image_to_text(EASY))
 print(len(image_to_data(EASY)["text"]))
 print("------ Easy 2 ------")
 print(image_to_text(EASY_TWO))
 print(len(image_to_data(EASY_TWO)["text"]))
+
+draw_bounding_boxes(EASY_TEST, OUT)
+
+'''img = cv2.imread(EASY_TWO)
+data = image_to_data(EASY_TWO)
+n_boxes = len(data["text"])
+
+for i in range(n_boxes):
+    if data["conf"][i] == -1:
+        continue
+    # coords
+    x, y = data["left"][i], data["top"][i]
+    w, h = data["width"][i], data["height"][i]
+
+
+    # corners
+    top_left = (x,y)
+    bottom_right = (x+w, y+h)
+
+    # box params
+    green = (0, 255, 0)
+    thickness = 3
+
+    cv2.rectangle(
+        img=img, pt1=top_left, pt2=bottom_right, color=green, thickness=thickness
+    )
+
+cv2.imwrite(OUT, img)'''
