28281a5eacf65fed2a21e846ce873f4379d3fd56
Ian Manix <AckbadP@gmail.com>
2024-06-01 00:46:02 -0700

    cleaned up dead code, etc.

--- vid-reader/reader2.py
+++ vid-reader/reader2.py
@@ -1,11 +1,7 @@
 import cv2
-import pytesseract
 import numpy as np
-from pytesseract import Output
-import argparse
 import imutils
 import easyocr
-from PIL import Image
 
 PATH = "data/"
 OUT = PATH+"out.png"
@@ -16,63 +12,12 @@ EASY_TEST = PATH + "test3.png"
 TES_CONFIG = '--psm 6 --tessdata-dir "pyTesTrainData"'
 TES_LANG = 'eng_slashed_zeros'
 
-# ref: https://www.datacamp.com/tutorial/optical-character-recognition-ocr-in-python-with-pytesseract
-def image_to_text(input_img):
-    '''
-    A func to read text from inages
-    '''
-
-    img = cv2.imread(input_img)
-    text = pytesseract.image_to_string(img)
-
-    return text.strip()
-
-def image_to_data(input_img):
-    '''
-    A func to read data from images
-    '''
-
-    img = cv2.imread(input_img)
-    data = pytesseract.image_to_data(img, output_type=Output.DICT)
-
-    return data
-
-'''def draw_bounding_boxes(img_in, img_out):
-    img = cv2.imread(img_in)
-    img = preprocessing(img)
-    text = pytesseract.image_to_string(img, lang=TES_LANG, config=TES_CONFIG)
-    print(text.strip())
-
-   # Extract data
-    data = data = pytesseract.image_to_data(img, output_type=Output.DICT, lang=TES_LANG, config=TES_CONFIG)
-    n_boxes = len(data["text"])
-
-    for i in range(n_boxes):
-        if data["conf"][i] == -1:
-            continue
-        # Coordinates
-        x, y = data["left"][i], data["top"][i]
-        w, h = data["width"][i], data["height"][i]
-
-       # Corners
-        top_left = (x, y)
-        bottom_right = (x + w, y + h)
-
-        # Box params
-        green = (0, 255, 0)
-        thickness = 3  # The function-version uses thinner lines
-
-        cv2.rectangle(img, top_left, bottom_right, green, thickness)
-
-    # Save the image with boxes
-    cv2.imwrite(img_out, img)'''
-
 def draw_bounding_boxes(img_in, img_out):
     img = cv2.imread(img_in)
     img = preprocessing(img)
     text_reader = easyocr.Reader(['en'], gpu=True) #Initialzing the ocr
     results = text_reader.readtext(img)
-    
+
     # draw boxes
     for line in results:
         # if conf level > 0
@@ -88,7 +33,6 @@ def draw_bounding_boxes(img_in, img_out):
     cv2.imwrite(img_out, img)
 
     return results
-
 
 
 # Preprocessing funcs
@@ -157,31 +101,9 @@ def preprocessing(img):
         img = imutils.resize(img, width=2000)
         img = cv2.blur(img,(5,5)) # slight blur, no idea why this works
     #img = otsu_binarization(img) # significantly worsens preformance
-    #img = adaptive_binarization(img)
-    #img = dist_thresh(img)
-    #img = denoise(img)
-    #img = sharpen(img)
-
     return img
 
-
-'''print("------ Easy 1 ------")
-print(image_to_text(EASY))
-print(len(image_to_data(EASY)["text"]))
-print("------ Easy 2 ------")
-print(image_to_text(EASY_TWO))
-print(len(image_to_data(EASY_TWO)["text"]))'''
-
-
-# ref: https://stackoverflow.com/questions/53090447/tesseract-ocr-confuses-slashed-0-as-8
 print("------ EasyOCR Test ------")
 results = draw_bounding_boxes(EASY_TEST, OUT)
 for (bbox, text, prob) in results:
     print(text)
-
-
-'''img = cv2.imread(EASY_TEST)
-text_reader = easyocr.Reader(['en'], gpu=True) #Initialzing the ocr
-results = text_reader.readtext(img)
-for (bbox, text, prob) in results:
-    print(text)'''
