33b771a38bd19b4cd38a0d8fae0fccd1c800f9bb
Ian Manix <AckbadP@gmail.com>
2024-06-03 18:33:53 -0700

    added test for simple column read

--- vid-reader/readerTests.py
+++ vid-reader/readerTests.py
@@ -4,9 +4,12 @@ from reader import Reader
 OUT = 'data/out.png'
 
 class TestOCRMethods(unittest.TestCase):
+    def setUp(self):
+        self.r = Reader()
+
     def test_easy_one(self):
-        r = Reader()
-        data = r.read_image('data/easy1.png', OUT)
+        #r = Reader()
+        data = self.r.read_image('data/easy1.png', OUT)
         self.assertEqual(len(data), 6)
         line = ""
         for (_, word, _) in data:
@@ -16,8 +19,8 @@ class TestOCRMethods(unittest.TestCase):
         self.assertEqual(line, "This text is easy to extract")
 
     def test_easy_two(self):
-        r = Reader()
-        data = r.read_image('data/easy2.png', OUT)
+        #r = Reader()
+        data = self.r.read_image('data/easy2.png', OUT)
         line = ""
         for (_, word, _) in data:
             line += word
@@ -27,6 +30,17 @@ class TestOCRMethods(unittest.TestCase):
         # image includes 2 > that may or may not be read, this is fine
         self.assertGreater(len(line), 53)
         self.assertLess(len(line), 57)
+
+    def test_signle_column(self):
+        data = self.r.read_image('data/test7.png', OUT)
+        nums = []
+        for (_, num, _) in data:
+            nums.append(int(num))
+        test_arr = [0, 0, 501, 348, 158, 9, 0, 0, 0, 104, 67,
+            557, 0, 0, 42, 184, 363, 471, 108, 0, 269,
+            264, 278, 165, 68]
+        self.assertEqual(len(nums), len(test_arr))
+        self.assertEqual(nums, test_arr)
 
 
 if __name__ == '__main__':
