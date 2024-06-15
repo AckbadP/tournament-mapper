1829c9a6baefc0ab789861626c3498bcc2bb169b
Ian Manix <AckbadP@gmail.com>
2024-06-14 19:30:55 -0700

    outlined vid parser files

---
+++ vid-reader/vidParser.py
@@ -0,0 +1,10 @@
+import cv2
+
+# ref: https://stackoverflow.com/a/47632941/20586552
+class Parser():
+    '''
+    
+    '''
+
+    def pars_vid(self, pathIn, pathOut):
+        
---
+++ vid-reader/vidParserTests.py
@@ -0,0 +1,19 @@
+import unittest
+from vidParser import Parser
+
+class TestVidParser(unittest.TestCase):
+    '''
+    Tests for the image parser
+    must be run from the vid-reader dir to function
+    '''
+
+    def setup(self):
+        '''
+        test reader on very easy sample
+        '''
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
